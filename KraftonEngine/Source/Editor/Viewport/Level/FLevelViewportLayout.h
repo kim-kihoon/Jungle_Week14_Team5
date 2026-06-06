c98201def78fc9c8ff5db9e0d83645d8c6f6c7fd
Park SangHyeok <tkdgurehf00@gmail.com>
2026-06-07 02:52:48 +0900

    feat: port viewport context actions

--- KraftonEngine/Source/Editor/Viewport/Level/FLevelViewportLayout.h
+++ KraftonEngine/Source/Editor/Viewport/Level/FLevelViewportLayout.h
@@ -8,6 +8,7 @@
 
 class SSplitter;
 struct FVector;
+struct FRotator;
 class AActor;
 class FLevelEditorViewportClient;
 class FEditorViewportClient;
@@ -100,6 +101,14 @@ public:
 	};
 
 	AActor* SpawnPlaceActor(EViewportPlaceActorType Type, const FVector& Location);
+	bool CopySelectedActors();
+	bool PasteCopiedActors();
+	bool CutSelectedActors();
+	bool DuplicateSelectedActors();
+	bool DeleteSelectedActors();
+	bool MoveSelectedActorsToView();
+	bool AlignSelectedActorsWithView();
+	bool HasActorClipboard() const { return !ActorClipboard.empty(); }
 
 	void SetActiveViewport(FLevelEditorViewportClient* InClient);
 	FLevelEditorViewportClient* GetActiveViewport() const { return ActiveViewportClient; }
@@ -114,6 +123,13 @@ public:
 	static int32 GetSlotCount(EViewportLayout Layout);
 
 private:
+	enum class EViewportContextPopupMode : uint8
+	{
+		None,
+		PlaceActor,
+		ActorContext,
+	};
+
 	struct FViewportContextMenuState
 	{
 		bool bTrackingRightClick[MaxViewportSlots] = {};
@@ -123,6 +139,7 @@ private:
 		int32 PendingSpawnSlot = -1;
 		FPoint PendingPopupPos = {};
 		FPoint PendingSpawnPos = {};
+		EViewportContextPopupMode PendingPopupMode = EViewportContextPopupMode::None;
 	};
 
 	enum class EViewportLayoutTransition : uint8
@@ -147,9 +164,16 @@ private:
 	void RenderSharedViewportToolbar(float ToolbarLeft, float ToolbarTop, float ToolbarWidth);
 	void RenderViewportSlotToolbar(int32 SlotIndex);
 	void HandleViewportContextMenuInput(const FPoint& MousePos);
+	void RenderViewportContextPopup();
 	void RenderViewportPlaceActorPopup();
+	void RenderViewportActorContextPopup();
+	void RenderPlaceActorMenuItems(int32 SpawnSlot, const FPoint& SpawnPos);
 	bool TryComputePlacementLocation(int32 SlotIndex, const FPoint& ClientPos, FVector& OutLocation) const;
 	AActor* SpawnActorFromViewportMenu(EViewportPlaceActorType Type, const FVector& Location);
+	void ClearActorClipboard();
+	void SelectActors(const TArray<AActor*>& Actors);
+	FVector GetActiveViewLocation() const;
+	FRotator GetActiveViewRotation() const;
 
 	// 아이콘 텍스처
 	void LoadLayoutIcons(ID3D11Device* Device);
@@ -188,6 +212,7 @@ private:
 	FEditorPlayToolbarWidget PlayToolbar;
 	FEditorCameraPreviewWidget CameraPreviewWidget;
 	FViewportContextMenuState ContextMenuState;
+	TArray<AActor*> ActorClipboard;
 	bool bHasSavedWorldAxisVisibility = false;
 	bool SavedWorldAxisVisibility[MaxViewportSlots] = {};
 	bool SavedGridVisibility[MaxViewportSlots] = {};
